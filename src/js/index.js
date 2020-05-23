f7d411ac7c1e6601d3e142713bfd6d8c182cfd46
Daniel Santos <Dsantos0495@gmail.com>
2020-05-22 21:53:49 -0500

    Terminamos de desarrollar en menu desplegable y casi todos los estilos responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,36 @@
-const pricing = document.getElementById('pricing');
-const product = document.getElementById('product');
-const aboutUs = document.getElementById('about-us');
-const careers = document.getElementById('careers');
-const community = document.getElementById('community');
+
 const deployMenu = document.getElementById('nav-deploy');
 const closeMenu = document.getElementById('nav-deploy-close');
-const burguerMenu = document.getElementById('nav-hamburguesa');
-
-burguerMenu.addEventListener('click', openMenu);
-closeMenu.addEventListener('click', hideMenu);
-pricing.addEventListener('click', hideMenu);
-product.addEventListener('click', hideMenu);
-aboutUs.addEventListener('click', hideMenu);
-careers.addEventListener('click', hideMenu);
-community.addEventListener('click', hideMenu);
+const burgerMenu = document.getElementById('nav-hamburguesa');
+const mediaQueryMenuDeploy = matchMedia('(min-width: 1023px)');
 
 function openMenu(event) {
-  deployMenu.style.display = 'block';
-  closeMenu.style.display = 'block';
-  burguerMenu.style.display= 'none';
+  if(deployMenu.classList.contains('show')) {
+    deployMenu.classList.remove('show');
+    closeMenu.classList.remove('show');
+    burgerMenu.style.display = 'block';
+  } else {
+    deployMenu.classList.add('show');
+    closeMenu.classList.add('show');
+    burgerMenu.style.display= 'none';
+  }
 }
 
-function hideMenu() {
-  deployMenu.style.display = 'none';
-  closeMenu.style.display = 'none';
-  burguerMenu.style.display= 'block';
+const changeSize = (mQ) => {
+  if (mediaQueryMenuDeploy.matches) {
+    burgerMenu.removeEventListener('click', openMenu);
+    deployMenu.removeEventListener('click', openMenu);
+  } else {
+    burgerMenu.addEventListener('click', openMenu);
+    deployMenu.addEventListener('click', openMenu);
+  }
 }
+
+mediaQueryMenuDeploy.addListener(changeSize);
+changeSize(mediaQueryMenuDeploy);
+
+
+
+
+
+
